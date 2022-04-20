10a9c57af18202914901bbda89f0b53770a4b9fc
Daniel García <daniel@etereo.io>
2022-04-20 15:56:11 +0200

    Add webpack support

--- pkg/front/app/src/app.tsx
+++ pkg/front/app/src/app.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import Paragraph from './paragraph'
 
 function App() {
     const paragraphs = Array.from(Array(10).keys())
@@ -6,21 +6,13 @@ function App() {
     return (
         <div>
             <h1>hi there!</h1>
-                <Box
-                    sx={{
-                        filter: 'blur( 5px )',
-                        pointerEvents: 'none',
-                        height: '100%',
-                    }}
-                >
-                    {
-                        paragraphs.map((count) => {
-                            return (
-                            <p>Paragraph {count + 1}</p>
-                            ) 
-                        })
-                    }
-                </Box>
+            {
+                paragraphs.map((count) => (
+                    <div key={"id-"+count}>
+                        <Paragraph count={count}></Paragraph>
+                    </div>
+                ))
+            }
         </div>
     )
 }
