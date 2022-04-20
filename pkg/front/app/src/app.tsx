adc94cb07243ce7f695d4dae1a71211012edb272
Daniel García <daniel@etereo.io>
2022-04-20 15:20:27 +0200

    Add mui box to setup node_modules dependencies for esbuild

--- pkg/front/app/src/app.tsx
+++ pkg/front/app/src/app.tsx
@@ -1,16 +1,26 @@
+import { Box } from '@mui/material'
+
 function App() {
     const paragraphs = Array.from(Array(10).keys())
 
     return (
         <div>
             <h1>hi there!</h1>
-            {
-                paragraphs.map((count) => {
-                   return (
-                    <p>Paragraph {count + 1}</p>
-                   ) 
-                })
-            }
+                <Box
+                    sx={{
+                        filter: 'blur( 5px )',
+                        pointerEvents: 'none',
+                        height: '100%',
+                    }}
+                >
+                    {
+                        paragraphs.map((count) => {
+                            return (
+                            <p>Paragraph {count + 1}</p>
+                            ) 
+                        })
+                    }
+                </Box>
         </div>
     )
 }
